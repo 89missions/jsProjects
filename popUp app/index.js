2779b99a1b738042cf22663ee5f537f12b726b1d
Samuel Asamoah <152725315+89missions@users.noreply.github.com>
2024-10-25 16:46:38 +0000

    Add files via upload

---
+++ popUp app/index.js
@@ -0,0 +1,11 @@
+let popup = document.querySelector('#popup')
+
+
+function openPopup(){
+    popup.classList.add('openpopup')
+}
+
+
+function removepopup(){
+    popup.classList.remove('openpopup')
+}
